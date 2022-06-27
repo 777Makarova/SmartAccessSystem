5c2b2e7e39d36052b67fb490fcda2344f24f41d3
Andrew <afterhattt@gmail.com>
2022-06-27 11:32:15 +0300

    added login.twig, header.twig

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,13 +10,11 @@ use App\Repository\UserRepository;
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
-    public function index( UserRepository $userRepository  
-    ): Response
+    public function index( UserRepository $userRepository ): Response
     {
-        $userRole = $userRepository->getRolesByUser('4');
+
 
         return $this->render('user/index.html.twig', [
-        'roles'=>$userRole
         ]);
     }
 }
